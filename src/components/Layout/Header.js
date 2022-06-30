911c7bed477d6479fc0f61d8892e4cd2dde84911
Sandeep Yadav <sandeepuim10@gmail.com>
2022-06-30 17:26:38 +0530

    HEADER-IMAGE

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,13 +1,17 @@
 import React, { Fragment } from "react";
-import './Header.css'
+import "./Header.css";
+import mealsImage from "../assets/meals.jpg";
 import HeaderButton from "./HeaderButton";
 const Header = () => {
   return (
     <Fragment>
       <header>
         <h1>OrderFood</h1>
-        <HeaderButton/>
+        <HeaderButton />
       </header>
+      <div className="main-image">
+        <img src={mealsImage} alt='Meals image' />
+      </div>
     </Fragment>
   );
 };

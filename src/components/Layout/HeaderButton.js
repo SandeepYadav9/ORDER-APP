5bc48db76e24ff99ba1146636b75e1b9a1208eab
Sandeep Yadav <sandeepuim10@gmail.com>
2022-06-30 16:03:00 +0530

    HTML5-CSS3 HEADER HEADER BUTTON

--- src/components/Layout/HeaderButton.js
+++ src/components/Layout/HeaderButton.js
@@ -1,11 +1,16 @@
-import React from 'react'
-
+import React from "react";
+import CartIcon from "../Cart/CartIcon";
+import "./HeaderButton.css";
 const HeaderButton = () => {
   return (
-    <button>
-        YourCart
+    <button className="button">
+      <span className="icon">
+        <CartIcon/>
+      </span>
+      <span>Your Cart</span>
+      <span className="badge">3</span>
     </button>
-  )
-}
+  );
+};
 
-export default HeaderButton
+export default HeaderButton;

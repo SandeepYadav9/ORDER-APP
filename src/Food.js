9b09929fe194da280696abf60d693b271c086a9d
Sandeep Yadav <sandeepuim10@gmail.com>
2022-06-30 18:23:51 +0530

    MEALS-SUMMERY-CSS-HTML

--- src/Food.js
+++ src/Food.js
@@ -1,10 +1,12 @@
 import React, { Fragment } from 'react'
 import Header from './components/Layout/Header'
+import Meals from './components/Meals/Meals'
 
 const Food = () => {
   return (
     <Fragment>
       <Header/>
+      <Meals/>
     </Fragment>
   )
 }

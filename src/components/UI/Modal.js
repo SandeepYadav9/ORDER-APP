97425b0c00ef64231befcdac7a8af906fd353ba6
Sandeep Yadav <sandeepuim10@gmail.com>
2022-07-02 18:19:55 +0530

    Modal-Overlay-Portals

--- src/components/UI/Modal.js
+++ src/components/UI/Modal.js
@@ -5,12 +5,18 @@ import ReactDOM from "react-dom";
 const BackDrop = (props) => {
   return <div className="backdrop"/>;
 };
-
+const ModalOverLay = (props) => {
+  return (
+    <div className="modal">
+      <div className="content">{props.children}</div>
+    </div>
+  );
+};
 const rootElements=document.getElementById('overlay')
 const Modal = (props) => {
   return <div>
     {ReactDOM.createPortal(<BackDrop/>, rootElements)}
-   
+    {ReactDOM.createPortal(<ModalOverLay>{props.children}</ModalOverLay>, rootElements)}
   </div>;
 };
 
